ac69f53ef59b77c2bff70d00454dd05cbc9e58b4
braincee <kwesi938@gmail.com>
2022-07-14 17:46:59 +0100

    Add enumerable class and methods

---
+++ enumerable.rb
@@ -0,0 +1,21 @@
+module MyEnumerable
+    def all?
+        ans = true
+        each do |i|
+            ans = false unless yield i
+        end
+        puts ans
+    end
+    def any?
+        ans = false
+       each do |i|
+            ans = true if yield i
+        end
+        puts ans
+    end
+    def filter
+        arr = []
+        each { |n| arr.push(n) if yield(n) }
+        puts arr
+    end
+end
