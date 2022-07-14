7c8c60aefcf9a80ce391accf9547169a060286c1
Ogaga <iyaraogaga69@gmail.com>
2022-07-14 16:36:54 +0100

    Add Mylist Class

---
+++ MyList.rb
@@ -0,0 +1,13 @@
+class MyList
+    include MyEnumerable
+  
+    def initialize(*list)
+      @list = list
+    end
+  
+    # rubocop:disable Style/ExplicitBlockArgument
+    def each
+      @list.each { |item| yield item }
+    end
+    # rubocop:enable Style/ExplicitBlockArgument
+  end
