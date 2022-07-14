923613f1297088cb33f5a9d63cfa561486714412
rolandfbk <rolandfbk@gmail.com>
2022-07-14 19:05:59 +0200

    linters fixed

--- enumerable.rb
+++ enumerable.rb
@@ -1,21 +1,23 @@
 module MyEnumerable
-    def all?
-        ans = true
-        each do |i|
-            ans = false unless yield i
-        end
-        puts ans
+  def all?
+    ans = true
+    each do |i|
+      ans = false unless yield i
     end
-    def any?
-        ans = false
-       each do |i|
-            ans = true if yield i
-        end
-        puts ans
+    puts ans
+  end
+
+  def any?
+    ans = false
+    each do |i|
+      ans = true if yield i
     end
-    def filter
-        arr = []
-        each { |n| arr.push(n) if yield(n) }
-        puts arr
-    end
+    puts ans
+  end
+
+  def filter
+    arr = []
+    each { |n| arr.push(n) if yield(n) }
+    puts arr
+  end
 end
--- mylist.rb
+++ mylist.rb
@@ -2,19 +2,19 @@ require_relative 'enumerable'
 
 class MyList
   include MyEnumerable
-  
+
   def initialize(*members)
-      @list = members
+    @list = members
   end
-  
-  def each
-      @list.each { |item| yield item }
+
+  def each(&block)
+    @list.each(&block)
   end
 end
 
 list = MyList.new(1, 2, 3, 4)
-list.all? {|e| e < 5}
-list.all? {|e| e > 5}
-list.any? {|e| e == 2}
-list.any? {|e| e == 5}
-list.filter {|e| e.even?}
+list.all? { |e| e < 5 }
+list.all? { |e| e > 5 }
+list.any? { |e| e == 2 }
+list.any? { |e| e == 5 }
+list.filter(&:even?)
