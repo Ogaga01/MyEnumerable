6a1b4fcfbd362f6431e1d3fba3abf6e30008e2e0
Ogaga <iyaraogaga69@gmail.com>
2022-07-15 06:56:02 +0100

    Fix linters errors

--- mylist.rb
+++ mylist.rb
@@ -1,20 +1,15 @@
-require_relative 'enumerable'
+require_relative './my_enumerable'
 
 class MyList
   include MyEnumerable
 
-  def initialize(*members)
-    @list = members
+  def initialize(*list)
+    @list = list
   end
 
-  def each(&block)
-    @list.each(&block)
+  # rubocop:disable Style/ExplicitBlockArgument
+  def each
+    @list.each { |item| yield item }
   end
+  # rubocop:enable Style/ExplicitBlockArgument
 end
-
-list = MyList.new(1, 2, 3, 4)
-list.all? { |e| e < 5 }
-list.all? { |e| e > 5 }
-list.any? { |e| e == 2 }
-list.any? { |e| e == 5 }
-list.filter(&:even?)
